1d781e4d862fca0d1031e7c9d25595c1158ccdbc
altbdoor <lancersupraskyline@gmail.com>
2018-04-04 22:54:30 +0800

    failed to upload to api

--- public/script.js
+++ public/script.js
@@ -1,30 +1,6 @@
 var hasDecided = false;
 var timerObj;
 
-$(document.documentElement).pasteImageReader(function (result) {
-    console.log(result);
-});
-
-$('#reaction-container').dragend({
-    direction: 'horizontal',
-    page: 2,
-    onSwipeEnd: function () {
-        if (!hasDecided && this.page != 1) {
-            hasDecided = true;
-
-            var deltaTime = timerEnd();
-            $('.reaction-time').text(deltaTime);
-
-            $(this.activeElement).siblings().addClass('inactive');
-            this.destroy();
-        }
-    },
-    afterInitialize: function () {
-        $('#reaction-container').removeClass('invisible');
-        timerStart();
-    },
-});
-
 function timerStart () {
     timerObj = new Date();
 }
@@ -35,22 +11,80 @@ function timerEnd () {
     return delta;
 }
 
-$('#inputfile').on('change', function () {
-    var file = $(this)[0].files[0];
-    var formData = new FormData();
-    formData.append('name', 'test.jpg');
-    formData.append('file', file);
+var generateInputTimeout;
+$('#generate-input').on('keyup keydown paste', function () {
+    var url = $(this).val();
+    var out = $('#generate-output');
 
-    $.ajax({
-        url: 'https://uguu.se/api.php?d=upload-tool',
-        data: formData,
-        cache: false,
-        contentType: false,
-        processData: false,
-        method: 'POST',
-        success: function (data) {
-            console.log(data);
-        }
+    clearTimeout(generateInputTimeout);
+
+    if (url) {
+        generateInputTimeout = setTimeout(function () {
+            var img = new Image();
+            img.onload = function () {
+                $(out).val(window.location.href + '?url=' + encodeURIComponent(url));
+            };
+            img.onerror = function () {
+                $(out).val('Error parsing URL as image');
+            };
+            img.src = url;
+        }, 800);
+    }
+
+}).trigger('keyup');
+
+new ClipboardJS('[data-clipboard-target="#generate-output"]');
+
+if (/\?url=/.test(window.location.href)) {
+    $('#generate-container').addClass('d-none');
+    $('#reaction-container').removeClass('d-none');
+
+    $('#reaction-container').dragend({
+        direction: 'horizontal',
+        page: 2,
+        onSwipeEnd: function () {
+            if (!hasDecided && this.page != 1) {
+                hasDecided = true;
+
+                var deltaTime = timerEnd();
+                $('.reaction-time').text(deltaTime);
+
+                $(this.activeElement).siblings().addClass('inactive');
+                this.destroy();
+            }
+        },
+        afterInitialize: function () {
+            $('#reaction-container').removeClass('invisible');
+            timerStart();
+        },
     });
 
-});
+    var url = decodeURIComponent(window.location.href.split('?')[1].replace(/^url=/, ''));
+    var img = new Image();
+
+    img.onload = function () {
+        var main = $('#reaction-main');
+        var mainCount = 3;
+        var imgLoadInterval;
+
+        function countDownImgLoad () {
+            $(main).text('Ready, in ' + mainCount + ' seconds...');
+            mainCount--;
+
+            if (mainCount < 0) {
+                clearInterval(imgLoadInterval);
+                $(main).empty().append(
+                    '<img class="img-fluid" src="' + img.src + '">'
+                );
+                timerStart();
+            }
+        }
+        countDownImgLoad();
+
+        imgLoadInterval = setInterval(countDownImgLoad, 1000);
+    };
+    img.onerror = function () {
+        $('#reaction-main').text('Error parsing URL as image');
+    };
+    img.src = url;
+}
